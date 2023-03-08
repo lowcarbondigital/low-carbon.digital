fd761304f7c43dc3d29d560e35b61118e98e858b
Jack Lenox <j@cklenox.earth>
2023-03-08 10:29:10 +0000

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://low-carbon.digital',
+	integrations: [tailwind({
+		config: { applyBaseStyles: false },
+	})],
+});
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Low-Carbon Digital';
+export const SITE_DESCRIPTION = 'A digital consultancy that makes stunning and sustainable websites for clients of all sizes.'
