217c76a76a792ef492a7713cad4136d02007ff08
Jack Lenox <j@cklenox.earth>
2023-06-12 15:21:10 +0100

    Improve wording of site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
 export const SITE_TITLE = 'Low-Carbon Digital';
-export const SITE_DESCRIPTION = 'A digital consultancy that makes beautiful, sustainable websites for people and planet.'
+export const SITE_DESCRIPTION = 'A digital consultancy that is helping to make a beautiful, sustainable web for people and planet.'
