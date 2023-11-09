fc3dc33451e42479bf5f6593e971d90cb285697f
Jack Lenox <j@cklenox.earth>
2023-11-09 09:56:02 +0000

    Fix RSS feed and add better Webmention support for notes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,20 @@
 import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
-export function get(context) {
+export async function get(context) {
+	const notes = (await getCollection('notes')).sort(
+		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
+	);
 	return rss({
 		title: 'low-carbon.digital',
 		description: 'A digital consultancy that makes beautiful, sustainable websites for people and planet.',
 		site: context.site,
-		items: [],
-	})
+		items: notes.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			link: `/notes/${post.slug}/`,
+		})),
+		stylesheet: '/rss/styles.xsl',
+	});
 }
